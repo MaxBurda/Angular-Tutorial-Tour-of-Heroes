597c8a10adbd9fc2a7dacbfa766676afc3a70756
Maksym Burda <Maksym.Burda@playtech.com>
2017-06-13 17:36:14 +0300

    fix and two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
-  template: '<h1>{{title}}</h1><h2>{{hero}}</h2>',
+  template: `
+  <div class="container">
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details! </h2>
+    <div><label> id: </label> {{hero.id}} </div>
+    <div>
+      <label> name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public title = 'Tour of GHeroes';
-  public hero = 'Windstorm';
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
