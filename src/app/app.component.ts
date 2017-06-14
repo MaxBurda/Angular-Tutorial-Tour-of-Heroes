846a1396e941cf5abdb1156c04b5baaa0207d533
MaxBurda <maxburda@gmail.com>
2017-06-14 10:39:27 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
   name: string;
 }
 
+let HEROES: Hero[] = [
+      { "id": 11, "name": "Mr. Nice" },
+      { "id": 12, "name": "Narco" },
+      { "id": 13, "name": "Bombasto" },
+      { "id": 14, "name": "Celeritas" },
+      { "id": 15, "name": "Magneta" },
+      { "id": 16, "name": "RubberMan" },
+      { "id": 17, "name": "Dynama" },
+      { "id": 18, "name": "Dr IQ" },
+      { "id": 19, "name": "Magma" },
+      { "id": 20, "name": "Tornado" }
+    ];
+
 @Component({
   selector: 'app-root',
   template: `
@@ -12,8 +25,8 @@ export class Hero {
       <h1>{{title}}</h1>
       <h2>My Heroes</h2>
       <ul class="heroes">
-        <li *ngFor="#hero of heroes">
-          <span class="badge">{{hero.id}}</span>{{hero.name}}
+        <li *ngFor="let hero of heroes">
+          <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
       </ul>
       <h2>{{hero.name}} details! </h2>
@@ -28,20 +41,10 @@ export class Hero {
 })
 
 export class AppComponent {
-  public title = 'Tour of GHeroes';
+  public title = 'Tour of Heroes';
   public heroes = HEROES;
-
+  public hero: Hero = {
+       id: 1,
+       name: 'Windstorm'
+     };
 }
-
-let HEROES: Hero[] = [
-      { "id": 11, "name": "Mr. Nice" },
-      { "id": 12, "name": "Narco" },
-      { "id": 13, "name": "Bombasto" },
-      { "id": 14, "name": "Celeritas" },
-      { "id": 15, "name": "Magneta" },
-      { "id": 16, "name": "RubberMan" },
-      { "id": 17, "name": "Dynama" },
-      { "id": 18, "name": "Dr IQ" },
-      { "id": 19, "name": "Magma" },
-      { "id": 20, "name": "Tornado" }
-    ];
