70500f748d5a59d69fba571994ce9b1b590e4c08
MaxBurda <maxburda@gmail.com>
2017-06-14 18:50:45 +0300

    planned commit

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -5,19 +5,8 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-heroes',
-  template:
-  `
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ngFor="let hero of heroes"
-          [class.selected]="hero === selectedHero"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-      </ul>
-      <hero-detail [hero]="selectedHero"></hero-detail>
-  `,
-  styleUrls: ['./app.component.css'],
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
   providers: [HeroService]
 })
 
@@ -40,4 +29,5 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  
 }
