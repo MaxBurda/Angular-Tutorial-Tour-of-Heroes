9078d9ebe4ca71fa08b80c2909ec3fa9885d214f
MaxBurda <maxburda@gmail.com>
2017-06-19 10:32:16 +0300

    Routing

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
-
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService){}
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1,5));
+      .then(heroes => this.heroes = heroes.slice(1, 5));
   }
-
 }
